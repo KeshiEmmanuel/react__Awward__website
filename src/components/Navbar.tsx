17e9630a35f4eeec2b84a232772a7f562f236630
KeshiEmmanuel <keshichidera@gmail.com>
2025-01-06 13:33:47 +0100

    add: Navbar components added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,31 @@
 import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useWindowScroll } from "react-use";
+import gsap from "gsap";
 
 const Navbar = () => {
-    const navContainerRef = useRef(null);
+    const navContainerRef = useRef<HTMLDivElement>(null);
     const [isAudioPlaying, setIsAudioPlaying] = useState(false);
     const [isIndicatorActive, setIsIndicatorActive] = useState(false);
-    const NavItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isNavVisible, setIsNavVisible] = useState(true);
     const audioElementRef = useRef<HTMLAudioElement>(null);
-    const toggleAudioIndicator = () => {
-        setIsAudioPlaying((prev) => !prev);
-        setIsIndicatorActive((prev) => !prev);
-    };
+    const { y: currentScrollY } = useWindowScroll();
+    const NavItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+    useEffect(() => {
+        if (currentScrollY === 0) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.remove("floating-nav");
+        } else if (currentScrollY > lastScrollY) {
+            setIsNavVisible(false);
+            navContainerRef.current.classList.add("floating-nav");
+        } else if (currentScrollY < lastScrollY) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.add("floating-nav");
+        }
+        setLastScrollY(currentScrollY);
+    }, [currentScrollY, lastScrollY]);
 
     useEffect(() => {
         if (audioElementRef.current) {
@@ -24,6 +38,18 @@ const Navbar = () => {
             console.error("Error audio element is null");
         }
     }, [isAudioPlaying]);
+
+    useEffect(() => {
+        gsap.to(navContainerRef.current, {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2,
+        });
+    }, [isNavVisible]);
+    const toggleAudioIndicator = () => {
+        setIsAudioPlaying((prev) => !prev);
+        setIsIndicatorActive((prev) => !prev);
+    };
     return (
         <div
             ref={navContainerRef}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,24 @@ export default {
                 zentry: ["zentry", "sans-serif"],
                 general: ["general", "sans-serif"],
                 "circular-web": ["circular-web", "sans-serif"],
-                "robert-meduim": ["robert-meduim", "sans-serif"],
+                "robert-medium": ["robert-medium", "sans-serif"],
                 "robert-regular": ["robert-regular", "sans-serif"],
             },
-        },
-        colors: {
-            blue: {
-                50: "#DFDFF0",
-                75: "#DFDFF2",
-                100: "#F0F2FA",
-                200: "#010101",
-                300: "#4fb7dd",
-            },
-            violet: {
-                300: "#5724ff",
-            },
-            yellow: {
-                100: "#8e983f",
-                300: "#edff66",
+            colors: {
+                blue: {
+                    50: "#DFDFF0",
+                    75: "#dfdff2",
+                    100: "#F0F2FA",
+                    200: "#010101",
+                    300: "#4FB7DD",
+                },
+                violet: {
+                    300: "#5724ff",
+                },
+                yellow: {
+                    100: "#8e983f",
+                    300: "#edff66",
+                },
             },
         },
     },
